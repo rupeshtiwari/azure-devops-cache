8f720352675511b31577519e203e0a8279eacab6
rupeshtiwari <roopkt@gmail.com>
2021-01-19 13:32:59 -0500

    fix: changing color

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk');
+
+throw new Error('Simulating build error');
  
 console.log(chalk.blue('Hello world!'));
 
-console.log(chalk.red('Hello red!'));
+console.log(chalk.yellow('Hello yellow!'));
